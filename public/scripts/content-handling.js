8c377efad5aebe30af89d924b84241898797d421
Filip <mazfil@pm.me>
2023-02-12 12:16:51 +1100

    missing js

---
+++ public/scripts/content-handling.js
@@ -0,0 +1,10 @@
+function changeTab(tab){
+    var i, content, tablinks;
+
+    content = document.getElementsByClassName("content");
+    for (i = 0; i < content.length; i++){
+        content[i].style.display = "none";
+    }
+
+    document.getElementById(tab).style.display = "block";
+}
